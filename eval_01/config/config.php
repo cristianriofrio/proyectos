469fbd2f2695433d45fb3e62d1918afa90841ec1
cristianriofrio <cristianriofrio@hotmail.com>
2024-08-14 23:00:01 -0500

    Bdd

--- eval_01/config/config.php
+++ eval_01/config/config.php
@@ -6,7 +6,7 @@ class ClaseConectar
     private $host = "localhost";
     private $usuario = "root";
     private $pass = "root";
-    private $base = "recetas";
+    private $base = "recetario";
     public function ProcedimientoParaConectar()
     {
         $this->conexion = mysqli_connect($this->host, $this->usuario, $this->pass, $this->base);
