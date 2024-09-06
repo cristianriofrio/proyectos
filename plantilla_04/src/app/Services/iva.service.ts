d49551cc2bd827336ae824973b7ea160f292c2c0
cristianriofrio <cristianriofrio@hotmail.com>
2024-09-05 22:35:52 -0500

    12

---
+++ plantilla_04/src/app/Services/iva.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Iva } from '../Interfaces/iva';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IvaService {
+  apiurl = 'http://localhost/proyectos/mcv_03/controllers/iva.controller.php?op=';
+
+  constructor(private lector: HttpClient) {}
+
+  todos(): Observable<Iva[]> {
+    return this.lector.get<Iva[]>(this.apiurl + 'todos');
+  }
