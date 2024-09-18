d8cf081b028fe33f26df4d87f56cffd6f61ef444
cristianriofrio <cristianriofrio@hotmail.com>
2024-09-18 13:08:43 -0500

    Listo

--- eval_02/src/app/reporte/reporte.component.ts
+++ eval_02/src/app/reporte/reporte.component.ts
@@ -8,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './reporte.component.scss'
 })
 export class ReporteComponent {
+  verReporteProveedores() {
+    // Redirige a la URL del reporte en una nueva pestaña
+    window.open('http://localhost/proyectos/eval_01/reports/proveedores.report.php', '_blank');
+  }
 
+  verReporteProductos() {
+    // Redirige a la URL del reporte en una nueva pestaña
+    window.open('http://localhost/proyectos/eval_01/reports/productos.report.php', '_blank');
+  }
 }
